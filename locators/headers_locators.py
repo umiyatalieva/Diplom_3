6469a415932f18099439341447d1421d510705f5
umiyatalieva <163591425+umiyatalieva@users.noreply.github.com>
2024-06-10 16:57:48 +0300

    Delete locators/headers_locators.py

--- locators/headers_locators.py
+++
@@ -1,3 +0,0 @@
-class HeaderLocators:
-    CONSTRUCTOR_BTN = ('xpath', '//p[text()="Конструктор"]')  # кнопка "Конструктор"
-    ORDERS_FEED_BTN = ('xpath', '//p[text()="Лента Заказов"]')  # кнопка "Лента Заказов"
